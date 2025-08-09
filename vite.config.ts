234d271444586fa9a3b69e5a7a1295bf3710d345
Edu Moreno <e.m.morenolp@gmail.com>
2025-08-09 02:43:41 -0300

    mod base url vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/Casa-Suiza-Web/",
+  base: "/web/",
 });
