90bf0c16aa6f1445afcd1bfa1c465c4beaa8d2d6
Edu Moreno <e.m.morenolp@gmail.com>
2025-08-25 00:32:25 -0300

    update

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_BASE =
-  import.meta.env.VITE_API_BASE_URL || "https://backend-web-t0x2.onrender.com";
+  import.meta.env.VITE_API_BASE_URL ||
+  "http://localhost:3000/api/v1";
 
 export const api = axios.create({
   baseURL: API_BASE,
